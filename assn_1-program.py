9635e0ab6f336386708a3c6db47ecd365456c075
swaraj321 <86162577+swaraj321@users.noreply.github.com>
2022-08-18 10:09:10 +0530

    Add files via upload

---
+++ assn_1-program.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello python")
+
+
+# In[14]:
+
+
+a= input("enter the value for a")
+b= input("enter the value for b")
+c= float(a) +float(b)
+d=float(a) / float(b)
+d
+c
+
+
+
+
+
+# In[15]:
+
+
+c
+
+
+# In[16]:
+
+
+d
+
+
+# In[19]:
+
+
+base = input("enter  the base of the triangle")
+height = input("enter the height of the triangle")
+area = (float (base) * float (height)) / 2
+area
+
+
+# In[20]:
+
+
+a = input("enter the value for a")
+b= input("enter the value for b")
+c = a
+a=b
+b=c
+
+
+# In[21]:
+
+
+a
+
+
+# In[22]:
+
+
+b
+
+
+# In[23]:
+
+
+import random
+print (random.randint(0,9))
+
+
+# In[24]:
+
+
+print (random.randint(0,9))
+
+
+# In[ ]:
+
+
+
+
